3f858e42f36c5043e64bca1dab5f75baa22e8d60
Kevin Neema Brian <kevinbrian@cortex-moz.com>
2023-06-16 15:04:55 +0200

    📦 NEW: Init Commit

---
+++ app/home/_layout.tsx
@@ -0,0 +1,12 @@
+import { Box, Text } from "native-base";
+import Home from ".";
+
+export default function Layout() {
+  return (
+    <Box>
+      <Text>Layout</Text>
+      <Home />
+      <Text>Tabs</Text>
+    </Box>
+  );
+}
---
+++ app/home/index.tsx
@@ -0,0 +1,11 @@
+import { Box, NativeBaseProvider, Text } from "native-base";
+
+export default function () {
+  return (
+    <NativeBaseProvider>
+      <Box>
+        <Text>Hello welcome to Communi Alerta App</Text>
+      </Box>
+    </NativeBaseProvider>
+  );
+}
