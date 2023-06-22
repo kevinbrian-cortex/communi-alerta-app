7e73325ce1a1f158a57cb537103c0ca14c6e3feb
Kevin Neema Brian <kevinbrian@cortex-moz.com>
2023-06-22 06:25:59 +0200

    📦 NEW: home page

--- app/(home)/index.tsx
+++ app/(home)/index.tsx
@@ -1,21 +1,66 @@
-import { Box, Button, Center, Heading, Text } from "native-base";
 import { useRouter } from "expo-router";
+import {
+  Box,
+  Button,
+  Container,
+  HStack,
+  Heading,
+  Icon,
+  IconButton,
+  Image,
+  Text,
+} from "native-base";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function () {
   const router = useRouter();
   return (
-    <Center width={"full"} height={"full"}>
-      <Box>
-        <Heading>Communi Alerta App</Heading>
-        <Text>Hello welcome to Communi Alerta App</Text>
-        <Button
-          onPress={() => {
-            router.push("/login");
-          }}
-        >
-          Click Me Dude
-        </Button>
-      </Box>
-    </Center>
+    <Box
+      width={"full"}
+      height={"full"}
+      borderColor={"red.100"}
+      borderStyle={"dotted"}
+      borderWidth={1}
+    >
+      {/* Top bar */}
+      <HStack
+        bg="blue.500"
+        justifyContent="space-between"
+        alignItems="center"
+        w="full"
+      >
+        <HStack>
+          <Box p={3}>
+            <Image
+              source={require("../../assets/images/logo.png")}
+              w={10}
+              h={10}
+              alt="Logo"
+            />
+          </Box>
+        </HStack>
+
+        <HStack alignItems="center">
+          <IconButton
+            icon={<Icon size="xl" as={Ionicons} name="menu" color="white" />}
+          />
+        </HStack>
+      </HStack>
+
+      {/* Header */}
+      <HStack
+        bg="blue.500"
+        justifyContent="space-between"
+        alignItems="center"
+        w="full"
+        p={5}
+      >
+        <Heading textAlign={"center"} color={"white"}>
+          Juntos por uma comunidade segura
+        </Heading>
+      </HStack>
+
+
+    </Box>
   );
 }
